c27b170156a7e0309ddcdf244da1b7c8b2e1a72e
Robert <robert@steveej-x13s.home>
2025-04-22 22:36:54 +0200

    add gitlab ci yaml

---
+++ deploy/production-webhook-deployment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+SUCCEDED=$(curl -H "X-Gitlab-Token:$DEPLOY_TOKEN" -X POST $DEPLOY_PRODUCTION_URL | grep "Done!" | wc -l)
+
+if [ "$SUCCEDED" != 1 ] ; then
+  echo "Deployment failed!"
+  exit 1
+fi
+
+echo "Deployment succeeded"
